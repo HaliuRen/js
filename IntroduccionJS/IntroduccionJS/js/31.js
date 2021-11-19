101774d095d58f6acc7813dd0155f19b95cd6a4e
HaliuRen <emyeolren@gmail.com>
2021-11-19 17:56:04 -0600

    proyecto musica

--- IntroduccionJS/IntroduccionJS/js/31.js
+++ IntroduccionJS/IntroduccionJS/js/31.js
@@ -20,3 +20,4 @@ if (Notification.permission == 'granted') {
         body: 'Tienes un mensaje'
     })
 }
+
--- IntroduccionJS/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ IntroduccionJS/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -42,3 +42,139 @@ const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace); //agregando enlace como hijo de la clase navegacion
 
 console.log(nuevoEnlace);
+
+
+
+
+
+
+// Eventos
+
+console.log(1);
+
+window.addEventListener('load', function() { // load espera a que el JS y los archivps que dependen del HTML esten listos
+    console.log(2);
+});
+
+window.onload = function() { // load - lo mismo que el load de arriba
+    console.log(3);
+}
+
+// Se utilizara mas este load
+
+document.addEventListener('DOMContentLoaded', function(){ // solo espera por el HTML, pero no espera CSS o imagenes, el html se descarga mas rapido que el js
+    console.log(4);
+})
+
+console.log(5);
+
+
+// load con funciones 
+window.addEventListener('load', imprimir);
+
+function imprimir(){
+    console.log(6);
+}
+
+
+//scroll
+
+window.onscroll = function(){
+    console.log('scrolling...');
+}
+
+
+
+// // Seleccionar elementos y asociarles un evento
+
+// // ejemplo con el boton de enviar de un formulario , el evento click se recomienda usar, en imagenes, texto etc
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault(); //usarlo siempre en formularios
+
+//     console.log('enviando formulariio');
+// });
+
+
+
+//Eventos de los Inputs y Textarea
+
+//ejemplo- se ve en la consola lo que se esta escribiendo en el formulario
+
+
+// Validando formulario
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+
+// Evento de Submit 
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    // Validar el formulario
+    const { nombre, email, mensaje } = datos;
+    
+    if(nombre === '' || email === '' || mensaje === '' ) {
+        mostrarAlerta('Todos los campos son obligatorios', true);
+
+        return; // corta la ejecucion del codigo
+    }
+
+    mostrarAlerta('El mensaje ha sido enviado');
+    
+    // Enviar el formulario
+    console.log('Enviando Formulario');
+});
+
+
+// mostrando en consola lo que se ingresa por el formulario
+function leerTexto(e) {
+    // console.log(e.target.value);
+    datos[e.target.id] = e.target.value;
+
+    // console.log(datos);
+}
+
+// validando formulario para enviar alerta
+function mostrarAlerta(mensaje, error = null) {
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+
+    if (error) {
+        alerta.classList.add('error');
+    } else {
+        alerta.classList.add('correcto');
+    }
+
+    formulario.appendChild(alerta);
+
+    // Desaparezca despues de 5 segundos
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+}
+
+
+
+
+
+
+
+
+
+
+
